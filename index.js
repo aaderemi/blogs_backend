22240cbab5679d9b72737256ceb5dffd5dd78004
unknown <aaderemi1@gmail.com>
2022-05-18 17:03:01 +0300

    added procfile

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const app = require('./app')
-const http = require('http')
-const logger = require('./utils/logger')
-const config = require('./utils/config')
+const app = require("./app");
+const http = require("http");
+const logger = require("./utils/logger");
+const config = require("./utils/config");
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
-const PORT = config.PORT
-server.listen(PORT, ()=>{
-    logger.info('Server running on PORT: ', PORT)
-})
+const PORT = config.PORT || 3003;
+server.listen(PORT, () => {
+  logger.info("Server running on PORT: ", PORT);
+});
