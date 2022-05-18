8c6e136e5e308e48bf39a88dc5b4929c8c3276e0
unknown <aaderemi1@gmail.com>
2022-05-18 18:33:58 +0300

    https again

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const app = require("./app");
-const http = require("https");
+//const http = require("http");
 const logger = require("./utils/logger");
 const config = require("./utils/config");
 
-const server = http.createServer(app);
+//const server = http.createServer(app);
 
 const PORT = config.PORT || 3003;
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   logger.info("Server running on PORT: ", PORT);
 });
